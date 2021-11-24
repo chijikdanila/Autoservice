03776c991a781ab72d31659214aed7ad4a518511
Danila <4ijikdanila@mail.ru>
2021-11-24 23:27:59 +0300

    Update Registry form.

--- Autoservice/Program.cs
+++ Autoservice/Program.cs
@@ -17,7 +17,7 @@ namespace Autoservice
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Registry());
         }
     }
 }
