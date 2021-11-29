33b278f46d2327cc77713e7a662fe7294a7d1a03
Dubrovskiy-Konstantin <kd.5556600@gmail.com>
2021-11-29 18:11:51 +0300

    Added Entities

--- Autoservice/Entities/Car.cs
+++ Autoservice/Entities/Car.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Autoservice.Entities
+﻿namespace Autoservice.Entities
 {
     public class Car
     {        
---
+++ Autoservice/Entities/ServiceType.cs
@@ -0,0 +1,16 @@
+﻿namespace Autoservice.Entities
+{
+    public class ServiceType
+    {
+        public int id { get; set; }
+        public string TypeService { get; set; }
+        public string TimePeriod { get; set; }
+
+        public bool isFullyDescribed()
+        {
+            if (string.IsNullOrEmpty(TypeService)) return false;
+            if (string.IsNullOrEmpty(TimePeriod)) return false;
+            return true;
+        }
+    }
+}
